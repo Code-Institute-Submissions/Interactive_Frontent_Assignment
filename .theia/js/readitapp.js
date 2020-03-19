73e63960f2ac39f1f4113d501d208c16edc6b7c5
Moscoder99 <osullivan.martina@gmail.com>
2020-03-19 01:17:55 +0000

    gettrackid functionality added

--- .theia/js/readitapp.js
+++ .theia/js/readitapp.js
@@ -1,70 +1,66 @@
-/*
-$(function () {
-    $("form").on("submit", function (e) {
-        e.preventDefault();
-        // prepare the request
-        var request = api.musixmatch.com/ws/1.1/track.search({
-            q_artist: "justin bieber",
-            page_size: 3,
-            apikey: "AIzaSyBgjbnfvnlZIUHYT1QV6KwqicZtY6lPm8w"
-        });
-        // execute the request
-        request.execute(function (response) {
-            var results = response.result;
-            console.log(results);
-        });
-    });
-});
-
-function init() {
-    gapi.client.setApiKey("AIzaSyBgjbnfvnlZIUHYT1QV6KwqicZtY6lPm8w");
-    gapi.client.load("youtube", "v3", function () {
-        // yt api is ready
-    });
-}
-
-/*
-http://api.musixmatch.com/ws/1.1/track.search?q_artist=justin bieber&page_size=3&page=1&s_track_rating=desc
-d3bcfe7a78f2e28470389faa67a5631e
-*/
-
-var data;
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?apikey=d3bcfe7a78f2e28470389faa67a5631e&q_artist=justin&page_size=10");
-xhr.send();
-
-function gettracklist(tracklistoutput){
- tracklist = tracklistoutput;
-}
-
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        console.log (this.status);
-        console.log (this.responseText);
-        console.log (JSON.parse(this.responseText));
-        gettracklist (JSON.parse(this.responseText));
+function gettracklist(tracklist){
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?apikey=d3bcfe7a78f2e28470389faa67a5631e&q_artist=justin&page_size=10");
+    xhr.send();
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            tracklist(JSON.parse(this.responseText));
+        };
     };
 };
 /*
-
-var defaultClient = MusixmatchApi.ApiClient.instance;
-var key = defaultClient.authentications['key'];
-key.apiKey = "d3bcfe7a78f2e28470389faa67a5631e"; // {String}
-
-var albumId= 14250417; // {String}
-var opts = { 
-    'format': "jsonp", // {String} output format: json, jsonp, xml.
-    'callback': "callback" // {String} jsonp callback
-};
-
-var callback = function(error, data, response) {
-    if (error) {
-        console.error(error);
-    } else {
-        console.log('API called successfully. Returned data:' ,JSON.stringify(data,null,2));
-    }
-};
-(new MusixmatchApi.AlbumApi()).albumGetGet(albumId, opts, (error, data, response) => {
-    callback(error, data, response, "albumGetGet"); 
-});
+PARAMETERS
+track_id    The Musixmatch track id
+commontrack_id  The Musixmatch commontrack id
 */
+
+function getlyrics(tracklist){
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?apikey=d3bcfe7a78f2e28470389faa67a5631e&q_artist=justin&page_size=10");
+    xhr.send();
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            tracklist(JSON.parse(this.responseText));
+        };
+    };
+};
+
+function outputtracklist(tracklist) {
+    console.log(tracklist.message.body.track_list)
+    console.dir(tracklist)
+};
+
+function outputtracklisttohtml(tracklist) {
+    tracklist = tracklist.message.body.track_list
+    var select = document.getElementById("tracklistoutput");
+    console.log(tracklist.length);
+    for(var i = 0; i < tracklist.length; i++) {
+        var opt = tracklist[i];
+        var el = document.createElement("option");
+        el.text = opt.track.track_name;
+        el.value = opt.track.track_name;
+        el.innerHTML = opt.track.track_name;
+        select.appendChild(el);
+        console.log(el);
+    };
+};
+
+function gettrackid(tracklist){
+    tracklist = tracklist.message.body.track_list
+    var selectedIndex = document.getElementById("tracklistoutput").options.selectedIndex;
+    console.log("selectedIndex");
+    console.log(selectedIndex);
+    var trackid = tracklist[selectedIndex].track.track_id
+    console.log("trackid");
+    console.log(trackid); 	
+}
+
+$("#submitsearch").click(function(){ 
+    //gettracklist(outputtracklist);
+    gettracklist(outputtracklisttohtml);
+});
+
+$("#getlyrics").click(function(){ 
+    console.log("tesymos");
+    gettracklist(gettrackid);
+});
