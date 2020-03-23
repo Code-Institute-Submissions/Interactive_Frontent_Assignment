63ff09e6a873da543b23e5781bccb22e953f2172
Moscoder99 <osullivan.martina@gmail.com>
2020-03-23 01:06:57 +0000

    tracksearch function wokring, getlyrics to be checked

--- .theia/js/readitapp.js
+++ .theia/js/readitapp.js
@@ -1,5 +1,6 @@
 $( document ).ready(function() {
-function gettracklist(callback) {
+
+    function gettracklist(callback) {
     var searchcriteria = document.getElementById("search").value;
     console.log("searchcriteria");
     console.log(searchcriteria);
@@ -7,21 +8,33 @@ function gettracklist(callback) {
     console.log("searchURL");
     console.log(searchURL);  
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?apikey=d3bcfe7a78f2e28470389faa67a5631e&q_artist=justin&page_size=10");
-    //xhr.open("GET", searchURL);
-    xhr.send();
- // do {
-   xhr.onreadystatechange = function() {     
-        if (this.readyState == 4 && this.status == 200) {
-            callback(JSON.parse(this.responseText));      
+    console.log(xhr.status);
+    console.log(xhr.statusText);
+    xhr.onreadystatechange = function() {     
+        console.log("in the function");
+        console.log(xhr.status);
+        console.log(xhr.statusText);
+        if (this.readyState == 4 && this.status == 200 && this.responseText !== null) {
+            console.log(this.responseText);
+            let x = (JSON.parse(this.responseText));
+            callback(x);
             }
-        }
+        };
+    //xhr.open("GET", "https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?apikey=d3bcfe7a78f2e28470389faa67a5631e&q_artist=justin&page_size=10",true);
+    xhr.open("GET", searchURL, true);
+    
+    xhr.send();
     };
 
-function outputtracklisttohtml(data) {
-console.log("testy");
-        console.log(data);
-        tracklist = data.message.body.track_list;
+$("#submitsearch").click(function outputtracklisttohtml() {
+    console.log("testyonw");
+    //event.preventDefault();
+            gettracklist(function(data) {
+            
+            console.log("received data");
+            console.log(data);
+            console.log("testy");
+          tracklist = data.message.body.track_list;
     console.log("tracklist");
     console.log(tracklist);
     var select = document.getElementById("tracklistoutput");
@@ -41,9 +54,10 @@ console.log("testy");
             select.appendChild(el);
             console.log("el");
             console.log(el);
-            };
         };
     };
+    });
+});
 
 /*function outputtracklist(tracklist) {
     console.log(tracklist.message.body.track_list)
@@ -56,9 +70,7 @@ function getlyrics(trackid){
     var xhr = new XMLHttpRequest();
     var URL = "https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.lyrics.get?apikey=d3bcfe7a78f2e28470389faa67a5631e&track_id=" + trackid;
     console.log("URL");
-    console.log(URL);
-    var lyricsdisplay = document.getElementById("displaylyrics");
-    lyricsdisplay.innerHTML = "";        
+    console.log(URL);    
     xhr.open("GET", URL);
     xhr.send();
     xhr.onreadystatechange = function() {
@@ -68,27 +80,31 @@ function getlyrics(trackid){
             console.log(lyrics);
             lyrics = lyrics.message.body.lyrics.lyrics_body;
             console.log(lyrics);
-            lyricsdisplay.innerHTML = lyrics;
+            if(lyrics !== "") {
+                document.getElementById("displaylyrics").innerHTML = lyrics;
+            }
+            else {
+            document.getElementById("displaylyrics").innerHTML = "No lyrics for this track";
+            }
         };
     };
 };
 
 function gettrackid(tracklist){
+    //tracklist.preventDefault();
     tracklist = tracklist.message.body.track_list
     var selectedIndex = document.getElementById("tracklistoutput").options.selectedIndex;
     var trackid = tracklist[selectedIndex].track.track_id;
+    document.getElementById("track_name").innerHTML = tracklist[selectedIndex].track.track_name;
+    document.getElementById("album_name").innerHTML = tracklist[selectedIndex].track.album_name;
+    document.getElementById("artist_name").innerHTML = tracklist[selectedIndex].track.artist_name;
     console.log("trackid");
     console.log(trackid);
     getlyrics(trackid);
 };
-
-
-function tracksearch(){
-    gettracklist(outputtracklisttohtml);
-};
-
-
-$("#submitsearch").click(tracksearch);
-
+$("#getlyrics").click(function(){
+    (gettracklist(gettrackid));
 });
 //$("#getlyrics").click(gettracklist(gettrackid));
+});
+
