9b5f4025a88524619cda68970fdc0c1968d69c0a
Moscoder99 <osullivan.martina@gmail.com>
2020-03-24 12:38:11 +0000

    added glowing title

--- .theia/js/app.js
+++ .theia/js/app.js
@@ -39,34 +39,3 @@ function init() {
         // yt api is ready
     });
 }
-
-
-/*
-$(function () {
-    $("form").on("submit", function (e) {
-        e.preventDefault();
-        // prepare the request
-        var request = gapi.client.youtube.search.list({
-            part: "snippet",
-            type: "video",
-            q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
-            maxResults: 3,
-            order: "viewCount",
-            publishedAfter: "2015-01-01T00:00:00Z",
-            videoCategoryId: 10
-        });
-        // execute the request
-        request.execute(function (response) {
-            var results = response.result;
-            console.log(results);
-        });
-    });
-});
-
-function init() {
-    gapi.client.setApiKey("AIzaSyBgjbnfvnlZIUHYT1QV6KwqicZtY6lPm8w");
-    gapi.client.load("youtube", "v3", function () {
-        // yt api is ready
-    });
-}
-*/
