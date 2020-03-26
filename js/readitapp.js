20fa3623a608c8a3bae152286842775c8b41aff6
Moscoder99 <osullivan.martina@gmail.com>
2020-03-26 01:47:42 +0000

    Completed formatting

--- js/readitapp.js
+++ js/readitapp.js
@@ -1,39 +1,39 @@
 $( document ).ready(function() {
 
-function gettracklist(callback) {
-    var searchcriteria = document.getElementById("search").value;
-    var searchURL = "https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?apikey=d3bcfe7a78f2e28470389faa67a5631e&page_size=500&q_artist=" + searchcriteria;  
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {     
-        if (this.readyState == 4 && this.status == 200 && this.responseText !== null) {
-            let x = (JSON.parse(this.responseText));
-            callback(x);
-            }
-        };
-    xhr.open("GET", searchURL, true); 
-    xhr.send();
+    function gettracklist(callback) {
+        var searchcriteria = document.getElementById("search").value;
+        var searchURL = "https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?apikey=d3bcfe7a78f2e28470389faa67a5631e&page_size=500&q_artist=" + searchcriteria;  
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {     
+            if (this.readyState == 4 && this.status == 200 && this.responseText !== null) {
+                let x = (JSON.parse(this.responseText));
+                callback(x);
+                }
+            };
+        xhr.open("GET", searchURL, true); 
+        xhr.send();
     };
 
 $("#submitsearch").click(function outputtracklisttohtml() {
     gettracklist(function(data) {
-    tracklist = data.message.body.track_list;
-    var select = document.getElementById("tracklistoutput");
-    select.innerHTML = "";
-    for(var i = 0; i < tracklist.length; i++) {
-        var opt = tracklist[i];
-        if (opt.track.has_lyrics == 1) {
-            var el = document.createElement("option");
-            var trname = opt.track.track_name;
-            var trid = opt.track.track_id;
-            var artist = opt.track.artist_name;
-            var album = opt.track.album_name;
-            var trackinfo = trname + "     >" + artist + "    >" + album + "    >" + trid;
-            el.text = trackinfo;
-            el.value = trackinfo;
-            el.innerHTML = trackinfo;
-            select.appendChild(el);
+        tracklist = data.message.body.track_list;
+        var select = document.getElementById("tracklistoutput");
+        select.innerHTML = "";
+        for(var i = 0; i < tracklist.length; i++) {
+            var opt = tracklist[i];
+            if (opt.track.has_lyrics == 1) {
+                var el = document.createElement("option");
+                var trname = opt.track.track_name;
+                var trid = opt.track.track_id;
+                var artist = opt.track.artist_name;
+                var album = opt.track.album_name;
+                var trackinfo = trname + "     >" + artist + "    >" + album + "    >" + trid;
+                el.text = trackinfo;
+                el.value = trackinfo;
+                el.innerHTML = trackinfo;
+                select.appendChild(el);
+            };
         };
-    };
     });
 });
 
@@ -58,7 +58,7 @@ function getlyrics(){
                 document.getElementById("displaylyrics").innerHTML = lyrics;
             }
             else {
-            document.getElementById("displaylyrics").innerHTML = "Unfortunately no lyrics were found for this track!";
+            document.getElementById("displaylyrics").innerHTML = "Unfortunately lyrics are not available for this track!";
             }
         };
     };
--- js/watchitapp.js
+++ js/watchitapp.js
@@ -1,29 +1,17 @@
 $( document ).ready(function() {
+
     $("#search").click(function(e) {
-    e.preventDefault();      
-    var searchcriteria = document.getElementById("searchcriteria").value;
-    console.log("searchcriteria");
-    console.log(searchcriteria);
-    var searchURL = "https://www.googleapis.com/youtube/v3/search?maxResults=5&part=snippet&publishedAfter=2017-01-01T00%3A00%3A00Z&type=video&videoCategoryId=10&key=AIzaSyBgjbnfvnlZIUHYT1QV6KwqicZtY6lPm8w&q=" + searchcriteria;
-    console.log("searchURL");
-    console.log(searchURL);  
-    var xhr = new XMLHttpRequest();
-    console.log(xhr.status);
-    console.log(xhr.statusText);
-    xhr.onreadystatechange = function() {     
-        console.log("in the function");
-        console.log(xhr.status);
-        console.log(xhr.statusText);
+        e.preventDefault();      
+        var searchcriteria = document.getElementById("searchcriteria").value;
+        var searchURL = "https://www.googleapis.com/youtube/v3/search?maxResults=5&part=snippet&publishedAfter=2017-01-01T00%3A00%3A00Z&type=video&videoCategoryId=10&key=AIzaSyBgjbnfvnlZIUHYT1QV6KwqicZtY6lPm8w&q=" + searchcriteria;  
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {     
         if (this.readyState == 4 && this.status == 200 && this.responseText !== null) {
             let x = (JSON.parse(this.responseText));
-            console.log(x);
            for (var i = 0; i < x.items.length; i++) {
                 var item = x.items[i];
-                console.log(item.id.videoId)
                 videotitle = item.snippet.title;
-                console.log(videotitle)
                 videoid = item.id.videoId;
-                console.log(videoid)
                 var resultsoutput = document.getElementById("results");
                 var titleelement = document.createElement('titleelement');
                 var br1 = document.createElement('br');
